8b602ee08dccc59bcf47c3100b717575481783d4
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2025-08-27 01:34:56 +0300

    2

--- 1_main.py
+++ 1_main.py
@@ -1,7 +1,8 @@
 
 x = 200
 print(isinstance(x, int))  #return a boolean
-
+# print(type(x)==int)
+a,b,c=map(int,input().split(" "))
 # --------------------------------------
 #This is a comment
 """
@@ -12,11 +13,18 @@ more than just one line
 # --------------------------------------
 # >>> python --version
 # python hello.py 
-
+# !pip --version 
 # --------------------------------------
 import sys
 print(sys.version)
-# --------------------------------------
+
+
+# import platform
+# x = platform.system()
+# x = dir(platform)
+# print(x) 
+
+# ------------------[Variables]--------------------
 x = 4       # x is of type int
 
 x = 35e3
@@ -51,7 +59,7 @@ print(type(x))
 x = "Hello World" 	                            #str 	
 x = 20 	                                        #int 	
 x = 20.5 	                                    #float 	
-x = 1j                          	            #complex 	
+x = 1j            #3+5j               	        #complex 	 #c.real ,c.imag 
 x = ["apple", "banana", "cherry"] 	            #list 	
 x = ("apple", "banana", "cherry") 	            #tuple 	
 x = range(6) 	                                #range 	
@@ -145,11 +153,28 @@ def myfunc():
 myfunc()
 print("Python is " + x) 
 # -------------------random-------------------
+
 import random
-print(random.randrange(1, 10)) 
+print(random.random()) #between 0-1 size one element float
+print(random.randint(1,20)) #between a-1b size one element int
+print(random.uniform(1,20))#between a-1b size one element float
+
+print(random.randrange(10)) 
+print(random.randrange(1, 10))  #randrange(start, stop=None, step=1) size one element  int  EX: randrange(0,20,2) retutn even
+print(random.randrange(0,20,2))
+
+print(random.choice(['a','b','c']))#choice('sweet home alabama')# get one elment
+ 
+print(random.sample(range(200) ,10))#print 10 utem reandon integer
+
+items = [1,2,3,4,5,6]
+random.shuffle(items)#Shuffle list x in place, and return None.
+print(items)#[3, 6, 1, 5, 4, 2]
 # ------------------if-else--------------------
 # a == b   a != b
 # and  ,or , not a > b
+# Note:  a != b  ,and,or,not,, and == &    ,, or== |            Continue   break  is None ,is not None
+
 a = 200
 b = 33
 if b > a:
@@ -160,6 +185,9 @@ else:
   print("a is greater than b")
 
 if a > b: print("a is greater than b") 
+# if a > b and c > a:print("Both conditions are True")
+# if a > b or a > c:print("At least one of the conditions is True")
+# if not a > b:print("a is NOT greater than b")
 
 print("A") if a > b else print("B") 
 
@@ -167,6 +195,23 @@ print("A") if a > b else print("=") if a == b else print("B")
 
 if b > a:
   pass
+# **************************
+15 < a < 30 
+
+x=None
+if x is None:
+    print("None")
+elif x is not None:
+    print("not None")
+
+x=5
+y=7
+if (x==2) & (y==3):#and
+    print("None")
+elif (x==2) | (y==3):#or
+    print("not None")
+
+
 # ------------------match--------------------
 day = 4
 match day:
@@ -224,6 +269,21 @@ while i < 6:
 else:
   print("i is no longer less than 6")
 
+
+
+# i = 1
+# while i < 6:
+#   print(i)
+#   if i == 2:
+#     print(i)
+#     continue
+#   if i == 3:
+#     print(i)
+#     break
+#   i += 1 
+# else:
+#   print("i is no longer less than 6")  
+
 # --------------------for------------------
 # break continue
 fruits = ["apple", "banana", "cherry"]
@@ -244,8 +304,63 @@ for x in range(2, 30, 3):
 else:
   print("Finally finished!")   
 
+
+# for x in fruits:
+#   if x == "banana":
+#     break
+#   if x == "apple":
+#     continue
+#   print(x)
+
+
+
 for x in [0, 1, 2]:
   pass  
+
+
+
+# print(sum([k for k in range(20)]))
+# newlist = [ x**3 for x in range(12)]
+#           [3*x for x in [y**2 for y in range(10)]]
+# newlist = [x for x in fruits if "a" in x]
+# newlist = [x for x in fruits if x != "apple"]   #??
+# newlist  = [i for i in range(20) if i%3 ==0  and i %2 ==0 ]
+# newlist = [[0 for i in range(5)] for j in range(7)]
+# newlist = [x if x != "banana" else "orange" for x in fruits]  #??
+# newlist = [(x, y) for x in [1,2,3] for y in [3,1,4] if x != y]
+
+
+
+# grades  = {"ahmed":35 ,"mona":40 ,"mena":37 }
+# for a in grades : print(a)# output -> ahmed  mona mena
+# for a in grades.items() : print(a)# output -> ('ahmed', 35) ,('mona', 40),('mena', 37)
+# for a,b in grades.items() : 
+# for a in grades.keys() : 
+# for a in grades.values() : 
+#-------------------------------
+# for index,val in enumerate(grades,3) : 
+#-------------------------------
+# students =  ["ahmed","ramy","ramy","mena"]
+# grades = [25,33,66,95,444]
+# for i,a in zip(students,grades) : 
+#  print("student" + i + " got " + str(a) + "degree"  )
+
+#--------------------- [public function] ---------------------
+
+# #dir(m) for show all premter and verible  import math as m 
+# sorted
+# list(reversed([1,23]))
+# del #delete
+# chr(10) #\n
+# bin(3) #0b11
+# oct(8)#001000 => [001][000]
+# ord('a')#97
+# abs(-20)  #20
+# divmod(102,2) #(51, 0)
+# pow(2,3) #8
+# min(5, 10, 25)
+# max(5, 10, 25)
+# round(1.25) #1
 # -----------------Functions---------------------
 # **************************
 def my_function(country = "Norway"):
@@ -345,6 +460,8 @@ print(mydoubler(11))
 print(mytripler(11))
 
 # ------------------ try-except---------------------
+# IOError , ValueError , ImportError , EOFError , KeyboadInterrupt
+
 try:
   print(x)
 except:
@@ -356,6 +473,19 @@ except NameError: #NameError and another for other errors
   print("Variable x is not defined")
 except:
   print("Something else went wrong")
+# **************************
+
+try:
+  print(x)
+except NameError:
+  print("Variable x is not defined")
+except ZeroDivisionError as err:
+    print("Error message is:", err)  
+except:
+  print("Something else went wrong")   
+finally : 
+    print ("end")
+
 # **************************
 try:
   print("Hello")
@@ -370,6 +500,18 @@ except:
   print("Something went wrong")
 finally:#The finally block, if specified, will be executed regardless if the try block raises an error or not.
   print("The 'try except' is finished") 
+# **************************
+
+try:
+  print("Hello")
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")  
+finally:                                     #The finally block, if specified, will be executed regardless if the try block raises an error or not.
+  print("The 'try except' is finished")  
+
+
 # **************************
 try:
   f = open("demofile.txt")
@@ -389,6 +531,11 @@ if x < 0:
 x = "hello"
 if not type(x) is int:
   raise TypeError("Only integers are allowed") 
+
+try:
+  raise Exception("Sorry, no numbers below zero")  #??  
+except:
+  print("Something went wrong")
 # **************************
 
 
@@ -488,8 +635,8 @@ print(myorder.format(carname = "Ford", model = "Mustang"))
 
 
 # -----------------input---------------------
-
-
+a,b,c=map(int,input().split(" "))
+# **************************
 y = True
 while y == True:
   x = input("Enter a number:")
